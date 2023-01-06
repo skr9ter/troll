edb2c8738a0c5fc9a56dae84c83b117cddf737a6
skr9ter <121878750+skr9ter@users.noreply.github.com>
2023-01-06 08:27:08 -0500

    Update 123.lua

--- 123.lua
+++ 123.lua
@@ -1,101 +1,148 @@
-local mouse = game:GetService("Players").LocalPlayer:GetMouse()
-local hold=false
-
-local FOV = Drawing.new("Circle")
-FOV.Visible = true
-FOV.Color = Color3.fromRGB(255, 0, 0)
-FOV.Thickness = 1
-FOV.NumSides = 100
-FOV.Radius = 100 --
-FOV.Filled = false
-
-
-
---settings
-local keybind=Enum.KeyCode.E
-local enablepconsole=false
-local teamcheck=false
-
---here is a visible check. this only functions in games 3rd person games or anything where your camera is not obscured.
-local function inlos(p, ...) -- In line of site? p == player head, ... character
-return #workspace.CurrentCamera:GetPartsObscuringTarget({p}, {workspace.CurrentCamera, game.Players.LocalPlayer.Character, ...}) == 0
+if not (syn or getexecutorname()) then
+    return;
 end
 
+local GetService = game.GetService;
+local Debris = game:GetService("Debris")
+local Players, RunService, UserInputService = GetService(game, "Players"), GetService(game, "RunService"),
+    GetService(game, "UserInputService");
 
-local esp = { }
+local LocalPlayer, Camera = Players.LocalPlayer, workspace.CurrentCamera
+local Mouse = LocalPlayer:GetMouse()
 
-local function NewQuad(thickness, color)
-local quad = Drawing.new("Quad")
-quad.Visible = false
-quad.PointA = Vector2.new(0,0)
-quad.PointB = Vector2.new(0,0)
-quad.PointC = Vector2.new(0,0)
-quad.PointD = Vector2.new(0,0)
-quad.Color = color
-quad.Filled = false
-quad.Thickness = thickness
-quad.Transparency = 0.9
-return quad
+local Framework = {
+    Target = "Head",
+    Sensitivity = 0.15,
+    Fill = Color3.fromRGB(255, 112, 112),
+    Outline = Color3.fromRGB(255, 255, 255),
+    Radius = 100,
+    Distance = 100,
+    Keybind = Enum.KeyCode.E,
+    ScanFromHead = false,
+    Checks = {
+        Team = false,
+        Friend = true,
+        Visible = false
+    }
+}; Framework.__index = Framework;
+
+function Framework:Pass_Through(From, Target, RaycastParams_, Ignore_Table)
+    RaycastParams_.FilterDescendantsInstances = Ignore_Table
+    local Result = workspace:Raycast(From, (Target.Position - From).unit * 10000, RaycastParams_)
+    if Result then
+        local Instance_ = Result.Instance
+        if Instance_:IsDescendantOf(Target.Parent) then
+            Passed = true
+            return true
+        elseif Instance_.CanCollide == false or Instance_.Transparency == 1 then
+            if Instance_.Name ~= "Head" and Instance_.Name ~= "HumanoidRootPart" then
+                table.insert(Ignore_Table, Instance_)
+                Pass_Through(Result.Position, Target, RaycastParams_, Ignore_Table)
+            end
+        end
+    end
 end
 
+function Framework:IsVisible(Target, FromHead)
+    local Character = LocalPlayer.Character
+    if not Character then return false end
+    local Head = Character:FindFirstChild("Head")
+    if not Head then return false end
+    local RaycastParams_ = RaycastParams.new();
+    RaycastParams_.FilterType = Enum.RaycastFilterType.Blacklist;
+    local Ignore_Table = { Camera, LocalPlayer.Character }
+    RaycastParams_.FilterDescendantsInstances = Ignore_Table;
+    RaycastParams_.IgnoreWater = true;
+    local From = FromHead and Head.Position or Camera.CFrame.p
+    local Result = workspace:Raycast(From, (Target.Position - From).unit * 10000, RaycastParams_)
+    Passed = false
+    if Result then
+        local Instance_ = Result.Instance
+        if Instance_:IsDescendantOf(Target.Parent) then
+            return true
+        elseif Framework.Checks.Visible and Instance_.CanCollide == false or Instance_.Transparency == 1 then
+            if Instance_.Name ~= "Head" and Instance_.Name ~= "HumanoidRootPart" then
+                table.insert(Ignore_Table, Instance_)
+                Framework:Pass_Through(Result.Position, Target, RaycastParams_, Ignore_Table)
+            end;
+        end;
+    end;
+    return Passed;
+end;
 
-local plr = game.Players.LocalPlayer
-local camera = game.Workspace.CurrentCamera
-game:GetService("UserInputService").InputBegan:connect(function(input)
-if input.KeyCode == keybind then
-hold=true
+function Framework:IsTeammate(player)
+    return player.Team == LocalPlayer.Team
 end
+
+local held = false;
+UserInputService.InputBegan:Connect(function(input)
+    if input.KeyCode == Framework.Keybind then
+        held = true;
+    end
 end)
 
-game:GetService("UserInputService").InputEnded:connect(function(input)
-if input.KeyCode == keybind then
-hold=false
-for index, descendant in pairs(esp) do
-descendant:Remove()
-end
-esp = { }
-end
+UserInputService.InputEnded:Connect(function(input)
+    if input.KeyCode == Framework.Keybind then
+        held = false;
+    end
 end)
-local function round( n, to ) return math.floor(n/to + 0.5) * to end
 
-local close = nil
-local hum = plr.Character.HumanoidRootPart
-game:GetService("RunService").RenderStepped:Connect(function(step)
-local closest = nil
-close = nil
-local min = math.huge
-FOV.Position = Vector2.new(camera.ViewportSize.X/2, camera.ViewportSize.Y/2)
-if hold == true then
-for i, v in pairs(game.Players:GetChildren()) do
-if plr.Character and v.Character and v.Character ~= plr.Character then --and plr.TeamColor ~= v.TeamColor then
-if v.Character:FindFirstChild("Head",0) ~= nil then
-local plrpos,OnScreen = camera:WorldToViewportPoint(v.Character.Head.Position)
-if OnScreen==true then
-local distance = math.sqrt(math.pow(mouse.X - plrpos.X, 2), math.pow(mouse.Y - plrpos.Y, 2))
-local fovdistance = math.sqrt(math.pow(FOV.Position.X - plrpos.X, 2) + math.pow(FOV.Position.Y - plrpos.Y, 2))
-if fovdistance <= FOV.Radius then
-if distance < min then
-min = distance
-closest = v
-close = v
-end
-end end
-end end
+function Framework:GetClosestToCursor()
+    local target, min_distance = nil, math.huge;
+    for _, player in next, Players:GetPlayers() do
+        local character = player.Character;
+        if player ~= LocalPlayer and character and character:FindFirstChild("Head") then
+            local position, visible = Camera:WorldToScreenPoint(character.Head.Position)
+            if visible then
+                local distance = Vector2.new(position.x - Mouse.x, position.y - Mouse.y).magnitude
+                if distance < min_distance then
+                    min_distance = distance;
+                    target = player;
+                end;
+            end
+        end
+    end
 
+    return target, min_distance;
 end
 
-if closest~=nil and close~=nil and mouse.Target.Parent ~= close.Character then
-local vector, onScreen = workspace.CurrentCamera:WorldToScreenPoint(close.Character.Head.Position)
-if onScreen == true then
-local dist = (Vector2.new(vector.X, vector.Y) - Vector2.new(mouse.X,mouse.Y)).Magnitude
-local magnitudeX = round(mouse.X - vector.X, 1)
-local magnitudeY = round(mouse.Y - vector.Y, 1)
-local mag1 = round(-magnitudeX * 0.25, 2)
-local mag2 = round(-magnitudeY * 0.25, 2)
+local Connection, Target, Highlight = nil, nil, Instance.new("Highlight")
+Connection = RunService.RenderStepped:Connect(function()
+    local Checks = Framework.Checks
+    if held == true then
+        local distance;
+        Target, distance = Framework:GetClosestToCursor()
+        if Target ~= nil then
+            local Targets = Framework.Target or "Head"
 
-mousemoverel(mag1, mag2)
-game:GetService('RunService').RenderStepped:Wait()
-end
-end
-end
+            local Aimlock = Target.Character:FindFirstChild(Targets)
+            local Position, Visible = Camera:WorldToScreenPoint(Aimlock.Position)
+
+            if Visible then
+                if Framework.Radius < distance then
+                    return
+                end
+
+                if Checks.Friend and Target:IsFriendsWith(LocalPlayer.UserId) then
+                    return;
+                end
+
+                if Checks.Team and Target:IsTeammate(LocalPlayer.UserId) then
+                    return;
+                end
+
+                if Checks.Visible and not Framework:IsVisible(Target.Character.Head, Framework.ScanFromHead) then
+                    return;
+                end
+
+                Highlight.Parent = Aimlock.Parent;
+                Highlight.FillColor = Framework.Fill
+                Highlight.OutlineColor = Framework.Outline
+
+                mousemoverel((Position.X - Mouse.X) * Framework.Sensitivity, (Position.Y - Mouse.Y) * Framework.Sensitivity)
+            end
+        end
+    end
 end)
+
+return Framework
